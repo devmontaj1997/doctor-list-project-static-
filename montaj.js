22c859bd7a83066974eaf4bad3a7245cbbd99678
devmontaj1997 <sekhmontajulali1997@gmail.com>
2024-11-07 11:10:16 +0530

    redesign done

---
+++ montaj.js
@@ -0,0 +1,18 @@
+// Get the button and the element to show/hide
+const button = document.querySelector(".mybtn.btnRight");
+const singleDoctorArea = document.querySelector("#collapseMember3971");
+
+// Add click event listener to the button
+button.addEventListener("click", () => {
+  // Toggle the 'show' class
+  singleDoctorArea.classList.toggle("show");
+});
+// Get the new button and the mobile element to show/hide
+const mobileButton = document.querySelector(".mybtn.btnRight.mobilebtn");
+const mobileSingleDoctorArea = document.querySelector("#collapseMember3972");
+
+// Add click event listener to the new button
+mobileButton.addEventListener("click", () => {
+  // Toggle the 'show' class on the mobile element
+  mobileSingleDoctorArea.classList.toggle("show");
+});
